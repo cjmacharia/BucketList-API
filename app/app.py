47243f5dfcca0f3499342390eba60410d4146e75
cjmash <collomash4@gmail.com>
2017-07-25 08:42:40 +0300

    [add]create bucketlist function

--- app/app.py
+++ app/app.py
@@ -18,3 +18,40 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+
+    @app.route("/api/bucketlists/", methods=["POST", "GET"])
+    def create_bucketlists():
+        if request.method == "POST":
+            name = str(request.data.get('name', ''))
+            if name:
+                bucketlist = BucketList(name=name)
+                bucketlist.save()
+                response = jsonify({
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                })
+                response.status_code = 201
+                return response
+            else:
+                response = jsonify({
+                    'you need to fill the name field'
+                })
+                response.status_code = 403
+        else:
+            # GET
+            allbucketlists = BucketList.get_all()
+            # Return the bucket lists
+            content = []
+            for bucketlist in allbucketlists:
+                bucket = {
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                }
+                content.append(bucket)
+            response = jsonify(content)
+            response.status_code = 200
+            return response
