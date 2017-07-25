af965adf66c53e9ae9f03c0b924a607b49156983
cjmash <collomash4@gmail.com>
2017-07-25 10:06:22 +0300

    [create]item model

--- app/models.py
+++ app/models.py
@@ -35,3 +35,39 @@ class BucketList(db.Model):
         """
         db.session.delete(self)
         db.session.commit()
+
+class Item(db.Model):
+    """Model for my items"""
+
+    __tablename__ = "items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    bucketlist_id = db.Column(db.Integer, db.ForeignKey(BucketList.id))  
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+                              onupdate=db.func.current_timestamp())                  
+    def __init__(self, name,bucketlist_id):
+        self.name = name
+        self.bucketlist_id = bucketlist_id
+    
+    def save(self):
+        """
+        save an item
+        """
+        db.session.add(self)
+        db.session.commit()
+    @staticmethod
+    def get_all_items(self):
+        """
+        get all items
+        """
+        return Item.query.filter_by(bucketlist_id=BucketList_id)
+
+    def delete(self):
+        """
+        Delete an item
+        """
+        db.session.delete(self)
+        db.session.commit()                        
+        
