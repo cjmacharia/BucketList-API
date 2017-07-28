82cb176193c1949a4d29fa525d72b68fe3a56c34
cjmash <collomash4@gmail.com>
2017-07-28 03:08:09 +0300

    [Update] app file

--- app/app.py
+++ app/app.py
@@ -1,24 +1,59 @@
 import json
+from functools import wraps
+
+from flask import abort, g, jsonify, make_response, request
 from flask_api import FlaskAPI
-from flask import request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 
 # local import
 from instance.config import app_config
 
-# initialize sql-alchemy
+# initialize db
 db = SQLAlchemy()
 
 def create_app(config_name):
-    from models import BucketList
-    from models import Item
+    """function wraps the creation of a new Flask object, 
+    and returns it after it's loaded up with 
+    configuration settings using"""
 
+    from models import BucketList, Item, User
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
+    def auth_token(func):
+        @wraps(func)    
+        def wrapper(*args, **kwargs):
+            # Check for the authentication token
+            token = request.headers.get("Authorization")
+            if not token:
+                # If there's no token provided
+                response = {
+                    "message": "Register or log in to access this resource"
+                }
+                return make_response(jsonify(response)), 401
+
+            else:
+                # Attempt to decode the token and get the user id
+                access_token = token.split(" ")[1]
+                user_id = User.decode_token(access_token)
+
+                if isinstance(user_id, str):
+                    # User id does not exist so payload is an error message
+                    message = user_id
+                    response = jsonify({
+                        "message": message
+                    })
+
+                    response.status_code = 401
+                    return response
+
+                else:
+                    return func(user_id=user_id, *args, **kwargs)
+        
+        return wrapper
     @app.route("/api/bucketlists/", methods=["POST", "GET"])
     def create_bucketlists():
         if request.method == "POST":
