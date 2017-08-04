5c3c9da49d58f8f6ff680dc19e14c5de4228044d
cjmash <collomash4@gmail.com>
2017-08-04 14:42:59 +0300

    [refactor] code

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -12,21 +12,20 @@ class BucketlistTestCases(unittest.TestCase):
     def setUp(self):
         """Define test variables and initialize app."""
         self.app = create_app(config_name="testing")
-        #set up test client
+        #set up test client for the application
         self.client = self.app.test_client
-        self.bucketlist = {'name': 'Go for skydiving '} 
+        self.bucketlist = {'name': 'Go for skydiving '}
 
         # bind the app to the current context
         with self.app.app_context():
             # create all tables
-            db.create_all()    
+            db.create_all()
 
         # Register a that we will use to test
         self.user_data = json.dumps(dict({
             "username": "tuser",
             "email": "cjuser@test.com",
-            "password": "test"
-                }))
+            "password": "test"}))
         self.client().post("/api/bucketlists/auth/register/", data=self.user_data,
                            content_type="application/json")
 
@@ -39,56 +38,59 @@ class BucketlistTestCases(unittest.TestCase):
                                                data=self.login_info,
                                                content_type="application/json")
         self.access_token = json.loads(
-            self.login_result.data.decode())['access_token']   
-        print (self.access_token)      
-        self.headers =dict(Authorization="Bearer "+ self.access_token, content_type = "application/json")                                  
+            self.login_result.data.decode())['access_token']
+        self.headers = dict(Authorization="Bearer "+ self.access_token,
+                            content_type="application/json")
 
     def test_bucketlist_creation(self):
         """
         Test the creation of a bucketlist through the API via POST
         """
         result = self.client().post("/api/bucketlists/", headers=self.headers,
-        data=self.bucketlist)
-        self.assertEqual(result.status_code, 201) 
-         
+                                    data=self.bucketlist)
+        self.assertEqual(result.status_code, 201)
+
 
     def test_get_all_bucketlists(self):
         """Test API to get  bucketlists (GET request)."""
-        result = self.client().post('/api/bucketlists/', headers=self.headers, data=self.bucketlist)
+        result = self.client().post('/api/bucketlists/',
+                                    headers=self.headers, data=self.bucketlist)
         self.assertEqual(result.status_code, 201)
         result = self.client().get('/api/bucketlists/', headers=self.headers)
         self.assertEqual(result.status_code, 200)
-        self.assertIn('Go for skydiving', result.data.decode('utf-8')) 
+        self.assertIn('Go for skydiving', result.data.decode('utf-8'))
 
     def test_get_bucketlist_by_id(self):
         """Test API to get  bucketlist with its id (GET request)."""
-        result = self.client().post('/api/bucketlists/', headers=self.headers, data=self.bucketlist)
+        result = self.client().post('/api/bucketlists/',
+                                    headers=self.headers, data=self.bucketlist)
         self.assertEqual(result.status_code, 201)
-        result = self.client().get('/api/bucketlists/1/',headers=self.headers)
+        result = self.client().get('/api/bucketlists/1/', headers=self.headers)
         self.assertEqual(result.status_code, 200)
-            
-    def test_get_bucketlist_with_invalid_id(self):
+
+    def test_get_bucketlist_invalid_id(self):
         """Test API to get a non existing bucketlist """
-        result = self.client().post('/api/bucketlists/', data=self.bucketlist, headers=self.headers)
+        result = self.client().post('/api/bucketlists/',
+                                    data=self.bucketlist, headers=self.headers)
         self.assertEqual(result.status_code, 201)
         result = self.client().get('/api/bucketlists/29/', headers=self.headers)
         self.assertEqual(result.status_code, 404)
-    
+
     def test_edit_bucketlist(self):
         """Test API to edit an existing bucketlist (PUT request)"""
-        result = self.client().post('/api/bucketlists/',  headers=self.headers, data={
+        result = self.client().post('/api/bucketlists/', headers=self.headers, data={
             'name' :'play the guitar'
-        })   
+        })
         self.assertEqual(result.status_code, 201)
 
         result = self.client().put('/api/bucketlists/1/', headers=self.headers, data={
-            'name' :'play the guitar and the piano', 
+            'name' :'play the guitar and the piano',
         })
         self.assertEqual(result.status_code, 200)
         endresult = self.client().get('/api/bucketlists/1/', headers=self.headers)
         self.assertIn('play the guitar and the piano', endresult.data.decode('utf-8'))
-    
-    def test_edit_bucketlist_with_invalid_id(self):
+
+    def test_edit_bucketlist_nonexisting_bucketlist(self):
         """Test API to edit a non existing bucketlist """
         result = self.client().post('/api/bucketlists/', headers=self.headers, data=self.bucketlist)
         self.assertEqual(result.status_code, 201)
@@ -103,9 +105,9 @@ class BucketlistTestCases(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         #test if the bucket has been deleted
         result = self.client().get('/api/bucketlists/1/', headers=self.headers)
-        self.assertEqual(result.status_code, 404) 
+        self.assertEqual(result.status_code, 404)
 
-    def test_delete_bucketlist_with_invalid_id(self):
+    def test_delete_bucketlist_invalid_id(self):
         """Test API to delete a non existing bucketlist """
         result = self.client().post('/api/bucketlists/', headers=self.headers, data=self.bucketlist)
         self.assertEqual(result.status_code, 201)
@@ -122,5 +124,4 @@ class BucketlistTestCases(unittest.TestCase):
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
-
 
--- tests/test_items.py
+++ tests/test_items.py
@@ -1,11 +1,12 @@
 import unittest
-from app.app import create_app, db
 import json
+from app.app import create_app, db
+
 
 
 class BucketitemsTestCases(unittest.TestCase):
     """
-    Test cases for the items
+    Test cases for the items in a bucket list
     """
 
     def setUp(self):
@@ -13,20 +14,18 @@ class BucketitemsTestCases(unittest.TestCase):
         self.app = create_app(config_name="testing")
         #set up test client
         self.client = self.app.test_client
-        self.bucketlist = {'name': 'Go for skydiving'} 
+        self.bucketlist = {'name': 'Go for skydiving'}
         self.item = {'name': 'hawai  skies yeah'}
-
         # bind the app to the current context
         with self.app.app_context():
             # create all tables
-            db.create_all()    
+            db.create_all()
 
         # Register a that we will use to test
         self.user_data = json.dumps(dict({
             "username": "testuser",
             "email": "cj@test.com",
-            "password": "test"
-                }))
+            "password": "test"}))
         self.client().post("/api/bucketlists/auth/register/", data=self.user_data,
                            content_type="application/json")
 
@@ -39,75 +38,102 @@ class BucketitemsTestCases(unittest.TestCase):
                                                data=self.login_info,
                                                content_type="application/json")
         self.access_token = json.loads(
-            self.login_result.data.decode())['access_token']        
-        self.headers =dict(Authorization="Bearer "+ self.access_token, content_type = "application/json")         
+            self.login_result.data.decode())['access_token']
+        self.headers = dict(Authorization="Bearer "+ self.access_token,
+                            content_type="application/json")
 
     def test_create_item(self):
         """
         Test the creation of an item through the API via POST
         """
         result = self.client().post("/api/bucketlists/", headers=self.headers, data=self.bucketlist)
-        self.assertEqual(result.status_code, 201)  
-        res = self.client().post('/api/bucketlists/1/items/',headers=self.headers,  data=self.item)
+        self.assertEqual(result.status_code, 201)
+        res = self.client().post('/api/bucketlists/1/items/', headers=self.headers, data=self.item)
         self.assertEqual(res.status_code, 201)
-        
-    def test_create_item_with_invalid_bucket(self):    
-        result = self.client().post("/api/bucketlists/", headers=self.headers,  data=self.bucketlist)
-        self.assertEqual(result.status_code, 201)  
-        res = self.client().post('/api/bucketlists/30/items/', headers=self.headers,  data=self.item)
+
+    def test_create_item_nonexistence_bucketlist(self):
+        """
+        Test the creation of an item through the API via POST with an invalid bucketlist
+        """
+        result = self.client().post("/api/bucketlists/", headers=self.headers,
+                                    data=self.bucketlist)
+        self.assertEqual(result.status_code, 201)
+        res = self.client().post('/api/bucketlists/30/items/', headers=self.headers,
+                                 data=self.item)
         self.assertEqual(res.status_code, 404)
 
     def test_get_bucket_item_by_id(self):
+        """
+        Test get item by id
+        """
         result = self.client().post("/api/bucketlists/", headers=self.headers, data=self.bucketlist)
-        self.assertEqual(result.status_code, 201)  
-        res = self.client().post('/api/bucketlists/1/items/', headers=self.headers,  data=self.item)
+        self.assertEqual(result.status_code, 201)
+        res = self.client().post('/api/bucketlists/1/items/', headers=self.headers, data=self.item)
         self.assertEqual(res.status_code, 201)
-        the_item = self.client().get('/api/bucketlists/1/items/1/',headers=self.headers, )
+        the_item = self.client().get('/api/bucketlists/1/items/1/', headers=self.headers, )
         self.assertEqual(the_item.status_code, 200)
         self.assertIn('hawai  skies yeah', the_item.data.decode('utf-8'))
 
-    def test_get_bucket_item_with_invalid_id(self):
+    def test_get_bucket_item_invalid_id(self):
+        """
+        Test getting  an item through the API with an invalid id
+        """
         result = self.client().post("/api/bucketlists/", headers=self.headers, data=self.bucketlist)
-        self.assertEqual(result.status_code, 201)  
+        self.assertEqual(result.status_code, 201)
         res = self.client().post('/api/bucketlists/1/items/', headers=self.headers, data=self.item)
         self.assertEqual(res.status_code, 201)
         the_item = self.client().get('/api/bucketlists/1/items/20/', headers=self.headers, )
-        self.assertEqual(the_item.status_code, 404)       
+        self.assertEqual(the_item.status_code, 404)
 
-    def test_edit_item(self): 
+    def test_edit_item(self):
+        """
+        Test editting   an item through the API
+        """
         result = self.client().post("/api/bucketlists/", headers=self.headers, data=self.bucketlist)
         self.assertEqual(result.status_code, 201)
         res = self.client().post('/api/bucketlists/1/items/', headers=self.headers, data={
             'name':'go to the bermuda'})
         self.assertEqual(res.status_code, 201)
-        res = self.client().put('/api/bucketlists/1/items/1/', headers=self.headers,  data={
+        res = self.client().put('/api/bucketlists/1/items/1/', headers=self.headers, data={
             'name':'go to the bermuda with my family'
         })
         self.assertEqual(res.status_code, 200)
 
-    def test_edit_item_that_doesnot_exist(self):
+    def test_edit_nonexistent_item(self):
+        """
+        Test edit  an item through the API with an invalid id
+        """
         result = self.client().post('/api/bucketlists/', headers=self.headers, data=self.bucketlist)
-        self.assertEqual(result.status_code, 201)  
+        self.assertEqual(result.status_code, 201)
         res = self.client().put('/api/bucketlists/1/items/19/', headers=self.headers, data={
             'name':'go to the bermuda with my family'
         })
         self.assertEqual(res.status_code, 404)
 
     def test_delete_invalid_item_id(self):
+        """
+        Test delete  an item through the API with an invalid id
+        """
         result = self.client().post('/api/bucketlists/', headers=self.headers, data=self.bucketlist)
-        self.assertEqual(result.status_code, 201) 
+        self.assertEqual(result.status_code, 201)
+        res = self.client().post('/api/bucketlists/1/items/',
+                                 headers=self.headers, data=self.item)
+        self.assertEqual(res.status_code, 201)
+        res = self.client().delete('/api/bucketlists/1/items/18/',
+                                   headers=self.headers, data=self.item)
+        self.assertEqual(res.status_code, 404)
+
+    def test_delete_items_nonexistent_bucketid(self):
+        """
+        Test delete  an item through the API with an invalid bucketlist
+        """
+        result = self.client().post('/api/bucketlists/', headers=self.headers, data=self.bucketlist)
+        self.assertEqual(result.status_code, 201)
         res = self.client().post('/api/bucketlists/1/items/', headers=self.headers, data=self.item)
         self.assertEqual(res.status_code, 201)
-        res = self.client().delete('/api/bucketlists/1/items/18/', headers=self.headers, data=self.item)
-        self.assertEqual(res.status_code, 404)    
-
-    def test_delete_items_with_invalid_bucket_id(self):
-        result = self.client().post('/api/bucketlists/', headers=self.headers, data=self.bucketlist)
-        self.assertEqual(result.status_code, 201) 
-        res = self.client().post('/api/bucketlists/1/items/', headers=self.headers,  data=self.item)
-        self.assertEqual(res.status_code, 201)
-        res = self.client().delete('/api/bucketlists/19/items/1/', headers=self.headers,  data=self.item)
-        self.assertEqual(res.status_code, 404)   
+        res = self.client().delete('/api/bucketlists/19/items/1/',
+                                   headers=self.headers, data=self.item)
+        self.assertEqual(res.status_code, 404)
     def tearDown(self):
         """teardown all initialized variables."""
         with self.app.app_context():
@@ -118,5 +144,4 @@ class BucketitemsTestCases(unittest.TestCase):
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
- 
-        
+
