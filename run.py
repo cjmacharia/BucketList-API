2d32a1bbe77a8c68c958d6e09317397ce5659306
cjmash <collomash4@gmail.com>
2017-08-15 16:19:19 +0300

    [create] procfile

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 from app.app import create_app
+import os
 
 config_name = "development"
 app = create_app(config_name)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run('', port=port)
+
+
