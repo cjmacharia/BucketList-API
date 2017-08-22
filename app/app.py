3273dbafe5b2fabca598a228107a11473e5ab43b
cjmash <collomash4@gmail.com>
2017-08-22 14:47:33 +0300

    [update] heroku configurations

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ def create_app(config_name):
 
     from .models import BucketList, Item, User
     app = FlaskAPI(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
+    app.config.from_object(os.getenv(['APP_SETTINGS']))
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
