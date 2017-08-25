4fdfe39cdecd9547f9e3b40c75846dddea87783f
cjmash <collomash4@gmail.com>
2017-08-25 15:17:35 +0300

    [chore] update user functionalities

--- app/app.py
+++ app/app.py
@@ -322,10 +322,13 @@ def create_app(config_name):
         '''
         This endpoint creates a new items in a bucket list.
         '''
+        bucketlist = BucketList.query.filter_by(id=id).first()
+        if not bucketlist:
+            abort(404)
+
+
         if request.method == "POST":
             name = str(request.data.get("name"))
-            if not BucketList.exists(name, user_id):
-                    return make_response(jsonify(dict(message="item herre"), 409))
             if name == "":
                 response = jsonify({
                     'message': 'oops! you need to fill the name field'
