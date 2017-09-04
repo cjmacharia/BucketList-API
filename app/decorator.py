6f9e227f4ecfecd71a9a8b345abcd9e1889454c5
cjmash <collomash4@gmail.com>
2017-09-04 16:15:50 +0300

    [chore] catch Bearer error

--- app/decorator.py
+++ app/decorator.py
@@ -4,33 +4,39 @@ from .models import User
 
 def auth_token(func):
         """function that wraps a wrapper function"""
+
         @wraps(func)
         def wrapper(*args, **kwargs):
             # Check for the authentication token
-            token = request.headers.get("Authorization")
-            if not token:
-                # If there's no token provided
-                response = {
-                    "message": "Register or login to be able to view this page"
-                }
-                return make_response(jsonify(response)), 401
-
-            else:
-                # Attempt to decode the token and get the user id
-                access_token = token.split(" ")[1]
-                user_id = User.decode_token(access_token)
-
-                if isinstance(user_id, str):
-                    # User id does not exist so payload is an error message
-                    message = user_id
-                    response = jsonify({
-                        "message": message
-                    })
-
-                    response.status_code = 401
-                    return response
+            try:
+                token = request.headers.get("Authorization")
+                if not token:
+                    # If there's no token provided
+                    response = {
+                        "message": "Register or login to be able to view this page"
+                    }
+                    return make_response(jsonify(response)), 401
 
                 else:
-                    return func(user_id=user_id, *args, **kwargs)
+                    # Attempt to decode the token and get the user id
+                    access_token = token.split(" ")[1]
+                    user_id = User.decode_token(access_token)
+
+                    if isinstance(user_id, str):
+                        # User id does not exist so payload is an error message
+                        message = user_id
+                        response = jsonify({
+                            "message": message
+                        })
+
+                        response.status_code = 401
+                        return response
+
+                    else:
+                        return func(user_id=user_id, *args, **kwargs)
+            except Exception:
+
+                response = {'message':'the token has no bearer'}
+                return make_response(jsonify(response)), 401
 
         return wrapper
