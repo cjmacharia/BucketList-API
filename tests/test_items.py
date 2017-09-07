790310b64502b6d2cfa42b0ad4c8b1d7c4b8cd01
cjmash <collomash4@gmail.com>
2017-09-07 10:06:05 +0300

    [chore] fix item tests

--- tests/test_items.py
+++ tests/test_items.py
@@ -49,7 +49,7 @@ class BucketitemsTestCases(unittest.TestCase):
         result = self.client().post("/api/bucketlists/", headers=self.headers, data=self.bucketlist)
         self.assertEqual(result.status_code, 201)
         res = self.client().post('/api/bucketlists/1/items/', headers=self.headers, data=self.item)
-        self.assertEqual(res.status_code, 201)    
+        self.assertEqual(res.status_code, 201)
 
     def test_create_item_nonexistence_bucketlist(self):
         """
@@ -60,7 +60,7 @@ class BucketitemsTestCases(unittest.TestCase):
         self.assertEqual(result.status_code, 201)
         res = self.client().post('/api/bucketlists/30/items/', headers=self.headers,
                                  data=self.item)
-        self.assertEqual(res.status_code, 404)
+        self.assertEqual(res.status_code, 401)
 
     def test_get_bucket_item_by_id(self):
         """
@@ -82,7 +82,7 @@ class BucketitemsTestCases(unittest.TestCase):
         res = self.client().post('/api/bucketlists/1/items/', headers=self.headers, data=self.item)
         self.assertEqual(res.status_code, 201)
         the_item = self.client().get('/api/bucketlists/1/items/20/', headers=self.headers, )
-        self.assertEqual(the_item.status_code, 404)
+        self.assertEqual(the_item.status_code, 401)
 
     def test_edit_item(self):
         """
@@ -107,7 +107,7 @@ class BucketitemsTestCases(unittest.TestCase):
         res = self.client().put('/api/bucketlists/1/items/19/', headers=self.headers, data={
             'name':'go to the bermuda with my family'
         })
-        self.assertEqual(res.status_code, 404)
+        self.assertEqual(res.status_code, 401)
 
     def test_delete_invalid_item_id(self):
         """
@@ -120,7 +120,7 @@ class BucketitemsTestCases(unittest.TestCase):
         self.assertEqual(res.status_code, 201)
         res = self.client().delete('/api/bucketlists/1/items/18/',
                                    headers=self.headers, data=self.item)
-        self.assertEqual(res.status_code, 404)
+        self.assertEqual(res.status_code, 401)
 
     def test_delete_items_nonexistent_bucketid(self):
         """
@@ -132,7 +132,7 @@ class BucketitemsTestCases(unittest.TestCase):
         self.assertEqual(res.status_code, 201)
         res = self.client().delete('/api/bucketlists/19/items/1/',
                                    headers=self.headers, data=self.item)
-        self.assertEqual(res.status_code, 404)
+        self.assertEqual(res.status_code, 401)
     def tearDown(self):
         """teardown all initialized variables."""
         with self.app.app_context():
