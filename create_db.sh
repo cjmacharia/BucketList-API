b8174ec72628dc3feabba45067af6aa86d13a292
Collins Macharia <jamescollins.jcparty@gmail.com>
2018-04-23 19:16:27 +0300

    Update create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
-
+pg_config --version
 export PSQL_HOME=/Library/PostgreSQL/10.3
 export PATH=/Library/PostgreSQL/10.3/bin:$PATH
 export PGPASSWORD=cjmash
+echo "we are here "
 psql -U postgres -c create database test_db
 
