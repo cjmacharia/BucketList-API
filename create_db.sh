fdf7c305e2f8903d02b416462a5aaa591207e352
Collins Macharia <jamescollins.jcparty@gmail.com>
2018-04-25 13:43:25 +0300

    Update create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -1,8 +1,8 @@
-#!/bin/bash 
-pg_config --version
-export PSQL_HOME=/Library/PostgreSQL/10.3
-export PATH=/Library/PostgreSQL/10.3/bin:$PATH
-export PGPASSWORD=cjmash
-echo "we are here "
-psql -U postgres -c create database test_db
+#!/bin/bash
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/postgresql/9.5/main/pg_hba.conf
+sed -i 's/32/0/g' /etc/postgresql/9.5/main/pg_hba.conf
+sed -i '/listen_address/s/^#//g' /etc/postgresql/9.5/main/postgresql.conf
+sed -i '/unix_socket_permissions/s/^#//g' /etc/postgresql/9.5/main/postgresql.conf
+sed -i 's/localhost/*/g' /etc/postgresql/9.5/main/postgresql.conf
+service postgresql restart
 
