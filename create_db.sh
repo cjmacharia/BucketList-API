f22b2f16b0e69ae8fad032c0140c6e1193617088
Collins Macharia <jamescollins.jcparty@gmail.com>
2018-04-26 19:08:25 +0300

    Update create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -2,6 +2,7 @@
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/postgresql/9.5/main/pg_hba.conf
 sed -i 's/32/0/g' /etc/postgresql/9.5/main/pg_hba.conf
 sed -i 's/peer/trust/g' /etc/postgresql/9.5/main/pg_hba.conf
+sed -i 's/md5/trust/g' /etc/postgresql/9.5/main/pg_hba.conf
 sed -i '/listen_address/s/^#//g' /etc/postgresql/9.5/main/postgresql.conf
 sed -i '/unix_socket_permissions/s/^#//g' /etc/postgresql/9.5/main/postgresql.conf
 sed -i 's/localhost/*/g' /etc/postgresql/9.5/main/postgresql.conf
